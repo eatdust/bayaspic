ada150a39a6eee3130577702d53d8b2f4c3e2318
chris <chris@ringeval.com>
2013-06-20 18:59:50 +0200

    cleaning master

--- bayaspic/bayslowroll.f90
+++
@@ -1,8 +0,0 @@
-program bayslowroll
-  use nestprec
-  use nestwrap, only : nest_init_slowroll, nest_sample_slowroll
-  
-  call nest_init_slowroll()
-  call nest_sample_slowroll()
-
-end program bayslowroll
