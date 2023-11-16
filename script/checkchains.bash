1494721d1cc71574ff42621783fadd14408abb6e
chris <chris@ringeval.com>
2023-11-16 13:58:40 +0100

    Move script inside new script dir

--- script/checkchains.bash
+++ script/checkchains.bash
@@ -12,7 +12,7 @@ listnames=`cat $modelfile | awk '{print $1$2}'`
 
 for name in $listnames
 do
-    isthere=`ls $chaindir/*.ini | grep $name`
+    isthere=`ls $chaindir/*.ini | grep bayesinf_$name.ini`
     if [[ -z "$isthere" ]]; then
 	echo 'Missing model '$name
     else
